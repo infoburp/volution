9a9c910e3e8a51d9c64d60f6cdcffc500b620b8a
Graeme Wolfendale <graeme@farnsworth.(none)>
2014-05-07 03:58:39 +0100

    started code

---
+++ volution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	//initialise variables
+	string DNA;
+}
+
+int computefitness (DNA, originalimage)
+{
+	//compute the fitness of input DNA, i.e. how close is it to original image?
+	renderDNA(DNA);
+}
+
+int renderDNA (DNA)
+{
+	//render input DNA to an image
+}
+
+int mutateDNA (DNA,mutationtype)
+{
+	//mutate input DNA randomly according to mutation type
+}
