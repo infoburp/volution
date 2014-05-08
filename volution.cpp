c356d4f81d71d60c22ee63132a7aef6761a829ef
Graeme Wolfendale <graeme@farnsworth.(none)>
2014-05-08 19:31:19 +0100

    added opencl initialisation

--- volution.cpp
+++ volution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <CL/cl.hpp>
 using namespace std;
 
 int main ()
@@ -9,6 +10,31 @@ int main ()
 		public:
 		string genome;
 	}
+    //initialise opencl device
+    //get all platforms (drivers)
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(all_platforms.size()==0){
+        std::cout<<" No platforms found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
+ 
+    //get default device of the default platform
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+    if(all_devices.size()==0){
+        std::cout<<" No devices found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Device default_device=all_devices[0];
+    std::cout<< "Using device: "<<default_device.getInfo<CL_DEVICE_NAME>()<<"\n";
+ 
+ 
+    cl::Context context({default_device});
+ 
+    cl::Program::Sources sources;
 }
 
 int computefitness (DNA, originalimage)
