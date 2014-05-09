281b3b9f41d8d6b32560b9f147a2c22aad1f2f8c
Graeme Wolfendale <graeme@farnsworth.(none)>
2014-05-09 14:47:46 +0100

    boost.compute library

--- monacl.cpp
+++ monacl.cpp
@@ -1,17 +1,26 @@
-#include <iostream>
-#include <stdexcept>
-#include <vexcl/vexcl.hpp>
+#include <vector>
+#include <algorithm>
+#include <boost/compute.hpp>
 
-int main() {
-    vex::Context ctx( vex::Filter::Type(CL_DEVICE_TYPE_GPU) && vex::Filter::DoublePrecision );
+namespace compute = boost::compute;
 
-    if (!ctx) throw std::runtime_error("No devices available.");
+int main()
+{
+    // get the default compute device
+    compute::device gpu = compute::system::default_device();
 
-    // Print out list of selected devices:
-    std::cout << ctx << std::endl;
-}
+    // create a compute context and command queue
+    compute::context ctx(gpu);
+    compute::command_queue queue(ctx, gpu);
 
-//initialise variables
+    // generate random numbers on the host
+    std::vector<float> host_vector(1000000);
+    std::generate(host_vector.begin(), host_vector.end(), rand);
+
+    // create vector on the device
+    compute::vector<float> device_vector(1000000, ctx);
+
+    //initialise variables
 const size_t n = 1024 * 1024;
 vex::Context ctx( vex::Filter::Any );
 vex::vector<double> leaderDNA(ctx, n);
@@ -21,6 +30,14 @@ vex::vector<double> mutatedDNArender(ctx, n);
 vex::vector<double> originalimage(ctx, n);
 
 
+//load image into gpu memory
+   // copy data to the device
+    compute::copy(
+        host_vector.begin(),
+        host_vector.end(),
+        device_vector.begin(),
+        queue
+    );
 
 //create random leader dna
 
@@ -46,11 +63,29 @@ VEX_FUNCTION(double, renderDNA, (double, x)(double, y),
     //return % match
     );
 
+
+ 
+
+    // sort data on the device
+    compute::sort(
+        device_vector.begin(),
+        device_vector.end(),
+        queue
+    );
+
 //if more fit, overwrite leader dna
 
 if (renderDNA(mutatedDNA) > leaderDNAscore) 
 {
-	leaderDNA = mutatedDNA;
+    leaderDNA = mutatedDNA;
 }
+    // copy data back to the host
+    compute::copy(
+        device_vector.begin(),
+        device_vector.end(),
+        host_vector.begin(),
+        queue
+    );
 
-
+    return 0;
+}
